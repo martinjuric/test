302aca47c1a1293b31b60b74577bef0ccb04f69c
martinjuric <juricmartin@gmail.com>
2020-07-24 09:04:10 +0200

    Doba dan

---
+++ server.js
@@ -0,0 +1,43 @@
+//REST API demo in Node.js
+var express = require('express'); // requre the express framework
+var app = express();
+var fs = require('fs'); //require file system object
+
+// Endpoint to Get a list of users
+app.get('/getUsers', function(req, res){
+    fs.readFile(__dirname + "/" + "korisnik.json", 'utf8', function(err, data){
+        console.log(data);
+        res.end(data); // you can also use res.send()
+    });
+});
+
+
+var korisnik = {
+    "korisnik5": {
+        "id":5,
+        "name":"Ivan",
+        "account":0,
+        "pass":"select"
+      },
+} 
+/*
+//The addUser endpoint
+app.post('/addUser', function(req, res){
+    //Step 2: read existing users
+    fs.readFile(__dirname + "/" + "korisnik.json", 'utf8', function(err, data){
+        data = JSON.parse(data);
+        //Step 3: append user variable to list
+        data["korisnik5"] = korisnik["korisnik5"];
+        console.log(data);
+        res.end(JSON.stringify(data));
+    });
+});*/
+
+// Create a server to listen at port 8080
+var server = app.listen(8080, function(){
+    var host = server.address().address
+    var port = server.address().port
+    console.log("REST API demo app listening at http://%s:%s", host, port)
+})
+
+
